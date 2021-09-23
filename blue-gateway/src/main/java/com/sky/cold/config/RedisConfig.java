6853272a4240c868491bcaf9ef9cd91bc4ddece2
wangchaoa <Wa93@@>
2021-09-23 20:34:26 +0800

    网关白名单和redis配置

---
+++ blue-gateway/src/main/java/com/sky/cold/config/RedisConfig.java
@@ -0,0 +1,13 @@
+package com.sky.cold.config;
+
+import com.sky.cold.common.config.BaseRedisConfig;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Redis相关配置
+ * Created by macro on 2020/6/19.
+ */
+@Configuration
+public class RedisConfig extends BaseRedisConfig {
+
+}
